793d38920eba13c9759a2a0e869051251bc40681
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-02 16:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {eggs: '2', bagel: '1', creeamCheese: '2spreads'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
     return object;
 }
